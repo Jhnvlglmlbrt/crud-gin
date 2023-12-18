96dc3720455f78494416956db5f2a16922a3c12a
Jhnvlglmlbrt <user.mail0encrypt@gmail.com>
2023-12-18 16:08:12 +0300

    fix valid

--- data/request/update_tags_request.go
+++ data/request/update_tags_request.go
@@ -2,5 +2,5 @@ package request
 
 type UpdateTagsRequest struct {
 	ID   int    `validate:"required"`
-	Name string `validate:"required, min=1, max=200" json:"name"`
+	Name string `validate:"required,min=1,max=200" json:"name"`
 }
